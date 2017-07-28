246c9f3ded223ee71775478781be0cc28b1ebf09
timhsieh7 <30532571+timhsieh7@users.noreply.github.com>
2017-07-28 18:46:37 -0500

    Add files via upload

--- MessageTypeDepot.cpp
+++ MessageTypeDepot.cpp
@@ -15,25 +15,25 @@
 MessageTypeDepot::MessageTypeDepot(MessageContainer* mc, EventHandler& e)
 	:	_e(e)
 {
-	msg_type_map = new std::map<std::string, MessageContainer*>();
-	msg_type_map->insert(std::pair<std::string, MessageContainer*>("red", mc));
-	msg_type_map->insert(std::pair<std::string, MessageContainer*>("yellow", mc));
-	msg_type_map->insert(std::pair<std::string, MessageContainer*>("blue", mc));
+	_msg_type_map = new std::map<std::string, MessageContainer*>();
+	_msg_type_map->insert(std::pair<std::string, MessageContainer*>("red", mc));
+	_msg_type_map->insert(std::pair<std::string, MessageContainer*>("yellow", mc));
+	_msg_type_map->insert(std::pair<std::string, MessageContainer*>("blue", mc));
 }
 
 
 MessageTypeDepot::~MessageTypeDepot()
 { 
-	delete msg_type_map->find("red")->second;
-	delete msg_type_map;
+	delete _msg_type_map->find("red")->second;
+	delete _msg_type_map;
 
 }
 
 
-void MessageTypeDepot::getMessage(std::string& msg_type) {
+void MessageTypeDepot::getMessageByType(std::string& msg_type) {
 
 	const Message*	msgPtr;
-	MessageContainer* messageContainer = msg_type_map->find(msg_type)->second();
+	MessageContainer* messageContainer = _msg_type_map->find(msg_type)->second();
 	if (nullptr != messageContainer) {
 		msgPtr = messageContainer->getMessageRequest(msg_type);
 		_e.getMessageEvent(msgPtr);
